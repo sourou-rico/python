410658385d00e71a5ff3d98a5e5b78bd2bbaff67
rico <interstisrico@gmail.com>
2025-08-05 12:51:13 +0100

    done

---
+++ script.py
@@ -0,0 +1,13 @@
+# Tu es l'homme guido van Rossum
+# print ("Hello, World!")
+# print("Bonjour")
+
+# mot = "Hello, World!"
+
+# for x in mot:
+#     print(x)
+# print("Fin du script")
+
+import random
+nombre = random.randrange(1,10)
+print(nombre)
